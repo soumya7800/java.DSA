529bee613617cf198881d293d4422cdcc87cf424
soumya7800 <150522755+soumya7800@users.noreply.github.com>
2025-12-09 01:48:43 +0530

    fix commit lock and update files

---
+++ UserInput.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("You entered: " + n );
+    }
+}
