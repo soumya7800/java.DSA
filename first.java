458f75337ddd34ee6d14047b90dc214db7263712
soumya7800 <150522755+soumya7800@users.noreply.github.com>
2025-12-08 23:50:21 +0530

    Add initial implementation of first.java with Hello World print statement

---
+++ first.java
@@ -0,0 +1,7 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// THE ONLY PRINT STATEMENT IN THIS FILE SHOULD BE "Hello, World!" BUT IF I WRITE NEW ONLY PRINT METHOD NNO NEW LINE WILL BE THEIR BUT IF I WRITE NEW PRINTLN METHOD NEW LINE WILL BE THEIR
