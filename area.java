1923c57e459f1f1ff89d7b8d577e66fa4c727823
soumya7800 <150522755+soumya7800@users.noreply.github.com>
2025-12-09 01:08:50 +0530

    fix commit lock and update files

---
+++ area.java
@@ -0,0 +1,8 @@
+public class area {
+    public static void main(String[] args) {
+        double radius = 7.5;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
+    
+}
