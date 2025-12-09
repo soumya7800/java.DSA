804c521ecd3a0c3aac68ab1ba1fc4d952df699a8
soumya7800 <150522755+soumya7800@users.noreply.github.com>
2025-12-09 23:37:44 +0530

    auto sync test

--- UserInput.java
+++ UserInput.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
+
 public class UserInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
         int n = sc.nextInt();
         System.out.println("You entered: " + n );
     }
 }
+
+
+// This program reads an integer from user input and prints it out.
+// nextint() method of Scanner class is used to read the integer value.
+// To run this program, compile it and execute. Then input an integer when prompted.
+//   Scanner sc = new Scanner(System.in); // Create a Scanner object to read input from the console 
---
+++ variablerules.java
@@ -0,0 +1,5 @@
+public class variablerules {
+    public static void main(String[] args) {
+        System.out.println("Variable Rules in Java:");
+    }
+}
