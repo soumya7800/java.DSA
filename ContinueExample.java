a960e577293d12d245d37e0317666ec6a34fd7c3
soumya7800 <150522755+soumya7800@users.noreply.github.com>
2025-12-15 00:52:36 +0530

    auto sync test

---
+++ ContinueExample.java
@@ -0,0 +1,15 @@
+public class ContinueExample {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i % 2 != 0)
+                continue;
+
+            System.out.println(i);
+        }
+    }
+}
+
+
+// the countinue is just like the not it gives every time the opposite output as per the if else if and else it show the opposite output
