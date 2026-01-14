74422267957e22a2e3300e6893ad23a902746ebf
soumya7800 <150522755+soumya7800@users.noreply.github.com>
2026-01-14 23:59:23 +0530

    auto sync test

--- Array/Array_Operrations/Sort_0s_and_1s_2s.java
+++ Array/Array_Operrations/Sort_0s_and_1s_2s.java
@@ -2,25 +2,33 @@ package Array.Array_Operrations;
 
 public class Sort_0s_and_1s_2s {
     public static void main(String[] args) {
-        int  [] arr = {0,1,2,0,1,2,1,2,0,0};
-        int n = arr.length;
-        int notZeroes = 0, notOnes=0;
-        for(int i=0; i<n; i++)
-        {
-            if(arr[i]==0)
-                notZeroes++;
 
-            if(arr[i]==1)
-                notOnes++;
+        int[] arr = {0, 1, 2, 0, 1, 2, 1, 2, 0, 0};
+        int n = arr.length;
+
+        int countZero = 0, countOne = 0;
+
+        // First pass: count 0s and 1s
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0)
+                countZero++;
+            else if (arr[i] == 1)
+                countOne++;
         }
-        for(int i=0; i<n; i++)
-        {
-            if(i<notZeroes)
-                arr[1]=0;
-            else if(i<notZeroes+notOnes)
-                arr[i] =2;
+
+        // Second pass: overwrite array
+        for (int i = 0; i < n; i++) {
+            if (i < countZero)
+                arr[i] = 0;
+            else if (i < countZero + countOne)
+                arr[i] = 1;
             else
                 arr[i] = 2;
+        }
+
+        // Print result
+        for (int x : arr) {
+            System.out.print(x + " ");
         }
     }
 }
